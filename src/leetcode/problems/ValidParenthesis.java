0fe5fa6c3373c74030c73ee0a5c1187a86ca6b4c
DELL <DELL@Ritesh>
2022-05-19 20:32:45 +0530

    valid parenthesis

---
+++ src/leetcode/problems/ValidParenthesis.java
@@ -0,0 +1,28 @@
+package leetcode.problems;
+
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+	public static void main(String[] args) {
+		ValidParenthesis validParanthesis = new ValidParenthesis();
+		System.out.println(validParanthesis.isValid("()[]{}"));
+	}
+	
+	public boolean isValid(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (char c : s.toCharArray()) {
+			if (c == '(')
+				stack.push(')');
+			else if (c == '{')
+				stack.push('}');
+			else if (c == '[')
+				stack.push(']');
+			else if (stack.isEmpty() || stack.pop() != c)
+				return false;
+		}
+		return stack.isEmpty();
+	}
+	        
+
+}
